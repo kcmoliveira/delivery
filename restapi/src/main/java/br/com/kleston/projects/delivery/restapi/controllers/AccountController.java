64017d0f8e0fe77ecad44dcb06b7c2f1a87df7c7
kleverton.macedo <kleverton.macedo@gmail.com>
2018-06-19 14:16:23 -0300

    Commit

--- restapi/src/main/java/br/com/kleston/projects/delivery/restapi/controllers/AccountController.java
+++ restapi/src/main/java/br/com/kleston/projects/delivery/restapi/controllers/AccountController.java
@@ -2,10 +2,14 @@ package br.com.kleston.projects.delivery.restapi.controllers;
 
 import br.com.kleston.projects.delivery.model.dtos.AccountDTO;
 import br.com.kleston.projects.delivery.model.dtos.LoginDTO;
+import br.com.kleston.projects.delivery.model.exceptions.AccountAlreadyExistsException;
+import br.com.kleston.projects.delivery.model.exceptions.AuthenticationException;
 import br.com.kleston.projects.delivery.model.services.AccountService;
 import br.com.kleston.projects.delivery.model.util.JsonUtils;
+import br.com.kleston.projects.delivery.restapi.response.Response;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.eclipse.jetty.http.HttpStatus;
 import spark.Spark;
 
 import java.util.Date;
@@ -18,28 +22,51 @@ public class AccountController {
 
     public static void configureRoutes() {
         Spark.post( "/account/signup", (req, res) -> {
-            AccountDTO accountDTO = JsonUtils.convertFromJson( req.body(), AccountDTO.class );
+            try {
+                AccountDTO accountDTO = JsonUtils.convertFromJson(req.body(), AccountDTO.class);
 
-            accountDTO = new AccountService().signUp( accountDTO );
+                accountDTO = new AccountService().signUp(accountDTO);
 
-            final String jwtToken = Jwts.builder()
-                    .setSubject( accountDTO.getUsername() )
-                    .setExpiration( new Date( System.currentTimeMillis() + EXPIRATION_TIME) )
-                    .signWith( SignatureAlgorithm.HS512, SECRET )
-                    .compact();
+                return "";
+            } catch (Exception e) {
+                if( e instanceof AccountAlreadyExistsException ) {
 
-            res.status(200 );
-            res.header( HEADER_STRING, TOKEN_PREFIX + jwtToken );
+                }
 
-            return "";
+                return "";
+            }
         } );
 
         Spark.post( "/account/authenticate", (req, res) -> {
-            LoginDTO loginDTO = JsonUtils.convertFromJson( req.body(), LoginDTO.class );
+            try {
+                LoginDTO loginDTO = JsonUtils.convertFromJson(req.body(), LoginDTO.class);
 
-            AccountDTO accountDTO = new AccountService().authenticate(loginDTO);
+                AccountDTO accountDTO = new AccountService().authenticate(loginDTO);
 
-            return "";
+
+                final String jwtToken = Jwts.builder()
+                        .setSubject(accountDTO.getUsername())
+                        .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                        .signWith(SignatureAlgorithm.HS512, SECRET)
+                        .compact();
+
+                res.status( HttpStatus.ACCEPTED_202 );
+                res.header( HEADER_STRING, TOKEN_PREFIX + jwtToken );
+
+                return "";
+            } catch (Exception e) {
+                Response response = new Response();
+
+                if( e instanceof AuthenticationException ) {
+                    response.setStatusCode( HttpStatus.UNAUTHORIZED_401 );
+                    response.setMessage( e.getMessage() );
+                } else {
+                    response.setStatusCode( HttpStatus.INTERNAL_SERVER_ERROR_500 );
+                    response.setMessage( "Error authenticating." );
+                }
+
+                return response;
+            }
         } );
     }
 }
