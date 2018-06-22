2e874a3142c5f11d99171c06b99b103e7d70f530
kleverton.macedo <kleverton.macedo@gmail.com>
2018-06-22 18:08:02 -0300

    Commit

--- docker/mysql_scripts/ddl.sql
+++ docker/mysql_scripts/ddl.sql
@@ -1,8 +1,38 @@
 create database if not exists delivery;
 
-create table account (
+create table accounts (
   id bigint auto_increment primary key,
 	name varchar(255) not null,
 	username varchar(255) not null unique,
 	password varchar(255) not null
-)
+);
+
+create table restaurants (
+  id bigint auto_increment primary key,
+	name varchar(255) not null
+);
+
+create table products (
+  id bigint auto_increment primary key,
+	name varchar(255) not null,
+	price double not null,
+	id_restaurant bigint not null,
+	foreign key (id_restaurant) references restaurants(id)
+);
+
+create table orders (
+  id bigint auto_increment primary key,
+	id_account bigint not null,
+	id_restaurant bigint not null,
+	date_order datetime not null,
+	foreign key (id_account) references accounts(id),
+	foreign key (id_restaurant) references restaurants(id)
+);
+
+create table orders_foods (
+  id bigint auto_increment primary key,
+	id_order bigint not null,
+	id_product bigint not null,
+	foreign key (id_order) references orders(id),
+	foreign key (id_product) references products(id)
+);
